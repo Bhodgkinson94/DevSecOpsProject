5a258e6989d8e50f86b530ba38038e3c082aa4f0
BHodgkinson94 <Ben.Hodgkinson94@gmail.com>
2024-09-15 16:00:43 +0100

    update

--- Main.tf
+++ Main.tf
@@ -1,6 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "julytesting" {
-  bucket ="julytesting"
+resource "aws_s3_bucket" "julytesting24" {
+  bucket ="julytesting24"
   }
+  terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "julytesting24"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
