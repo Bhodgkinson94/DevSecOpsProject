ca4ae0c6d66813c3856ba138d425c27615d98cc2
BHodgkinson94 <Ben.Hodgkinson94@gmail.com>
2024-09-15 15:34:32 +0100

    name change

--- Main.tf
+++ Main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "julytest" {
-  bucket ="julytest"
+resource "aws_s3_bucket" "julytesting" {
+  bucket ="julytesting"
   }
